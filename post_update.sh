e170b45d9523ce2dccde727fac4029d346c4f8ec
asantini <augusto.santini@mirgor.com.ar>
2021-10-19 10:29:01 -0300

    First update bundle test

---
+++ post_update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Post-update script
+#
+# This script will be executed after completing all the file renaming
+# corresponding to the last update
+#
+
+printf "Post-update script started.\n"
+
+# Real work here
+
+printf "Post-update script finished.\n"
+
+exit 0
